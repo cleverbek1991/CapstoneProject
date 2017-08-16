7453d562c3f1cddee77cbd0babc55a0231406d2b
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-08-16 11:36:14 -0500

    gh-pages

---
+++ app/values/fb-creds.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.constant('FBCreds', {
+    apiKey: "AIzaSyD8RZQx-KL2ai7Uvtl4k6XG-fHFWnBvchQ",
+    authDomain: "manager-bc860.firebaseapp.com",
+    databaseURL: "https://manager-bc860.firebaseio.com",
+    googleMapApiKey: "AIzaSyAM264Pj11WeEnFjMQ87o4KkXjfjGzaSG8"
+});
